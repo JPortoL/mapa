428350a1b3e148030966ee3aff4294998e72757c
JPortoL <jporto@unal.edu.co>
2023-12-06 00:15:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def raiz():
-    return render_template("inicio.html")
+    return render_template("hola.html")
 
 if __name__ == "__main__":
     app.run(debug=False)
