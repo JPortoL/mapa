40095c87913dd1d84b95adc2e2b587f0f45de1a5
JPortoL <jporto@unal.edu.co>
2023-12-07 22:26:50 -0500

    fe

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask_compress import Compress
 
 app = Flask(__name__)
+Compress(app)  # Configura la compresión para la aplicación
 
 @app.route("/")
 def raiz():
-    return render_template("hola.html")
+    return render_template("mapa.html")
 
 if __name__ == "__main__":
     app.run(debug=False)
