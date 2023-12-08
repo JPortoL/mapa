5b79506ec5cb5fac233f8913d4a7c6e30062be76
JPortoL <jporto@unal.edu.co>
2023-12-07 22:31:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Compress(app)  # Configura la compresión para la aplicación
 
 @app.route("/")
 def raiz():
-    return render_template("mapa.html")
+    return render_template("inicio.html")
 
 if __name__ == "__main__":
     app.run(debug=False)
